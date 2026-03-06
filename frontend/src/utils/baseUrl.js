ae939818134105b3b97c5a3c8a8b9541f4c34d95
manjituaml <manjit.singh@uaml.in>
2026-03-06 11:26:25 +0530

    path fixed

--- frontend/src/utils/baseUrl.js
+++ frontend/src/utils/baseUrl.js
@@ -1,6 +1,6 @@
 
 
-export const baseUrl = `http://localhost:3000`;
-// export const baseUrl = `https://uamlbackend.onrender.com`;
+// export const baseUrl = `http://localhost:3000`;
+export const baseUrl = `https://uamlbackend.onrender.com`;
 export const baseUserUrlPrefix = `/uampi/users`;
 export const baseOrderUrlPrefix = `/uampi/orders`;
